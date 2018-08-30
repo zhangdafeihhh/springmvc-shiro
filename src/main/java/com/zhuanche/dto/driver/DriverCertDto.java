ef2f3af1402a97b458d2ab2c0e02b898a5412de6
hexinchun <hexinchun@01zhuanche.com>
2018-08-31 00:08:50 +0800

    add

--- src/main/java/com/zhuanche/dto/driver/DriverCertDto.java
+++ src/main/java/com/zhuanche/dto/driver/DriverCertDto.java
@@ -1,9 +1,13 @@
 package com.zhuanche.dto.driver;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class DriverCertDto {
-    private Long id;
+public class DriverCertDto implements Serializable{
+   
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
 
     private Byte type;
 
--- src/main/java/com/zhuanche/dto/driver/DriverVerifyDto.java
+++ src/main/java/com/zhuanche/dto/driver/DriverVerifyDto.java
@@ -1,9 +1,13 @@
 package com.zhuanche.dto.driver;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class DriverVerifyDto {
-    private Long id;
+public class DriverVerifyDto implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+
+	private Long id;
 
     private Long driverId;
 
