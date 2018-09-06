fd7f7695eac9e7a9f9de2e383639456882d4d65d
hexinchun <hexinchun@01zhuanche.com>
2018-09-07 01:27:27 +0800

    add

--- src/main/java/com/zhuanche/dto/driver/DriverJoinRecordDto.java
+++ src/main/java/com/zhuanche/dto/driver/DriverJoinRecordDto.java
@@ -1,9 +1,13 @@
 package com.zhuanche.dto.driver;
 
+import java.io.Serializable;
 import java.util.Date;
 
-public class DriverJoinRecordDto {
-    private Integer id;
+public class DriverJoinRecordDto implements Serializable{
+    
+	private static final long serialVersionUID = 1L;
+
+	private Integer id;
 
     private Integer driverVerifyId;
 
