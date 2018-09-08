e733cff6de2a9aa2677aec7edb924166398e7090
lunan <lunan@01zhuanche.com>
2018-09-08 15:11:11 +0800

    联调修改

--- src/main/java/mapper/mdbcarmanage/ex/CarDriverTeamExMapper.java
+++ src/main/java/mapper/mdbcarmanage/ex/CarDriverTeamExMapper.java
@@ -4,6 +4,9 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
+import com.zhuanche.common.database.DynamicRoutingDataSource;
+import com.zhuanche.common.database.MasterSlaveConfig;
+import com.zhuanche.common.database.MasterSlaveConfigs;
 import com.zhuanche.dto.CarDriverTeamDTO;
 import com.zhuanche.dto.driver.DriverTeamGroupDTO;
 import com.zhuanche.request.DriverTeamRequest;
@@ -12,15 +15,36 @@ import org.apache.ibatis.annotations.Param;
 import com.zhuanche.entity.mdbcarmanage.CarDriverTeam;
 
 public interface CarDriverTeamExMapper{
+
+	@SuppressWarnings("rawtypes")
+	@MasterSlaveConfigs(configs={
+			@MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+	} )
 	/**查询车队列表(根据城市ID，供应商ID、车队ID  )**/
 	List<CarDriverTeam> queryDriverTeam(@Param("cityIds") Set<String> cityIds, @Param("supplierIds") Set<String> supplierIds ,  @Param("teamIds") Set<Integer>  teamIds );
 
+	@SuppressWarnings("rawtypes")
+	@MasterSlaveConfigs(configs={
+			@MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+	} )
 	List<CarDriverTeamDTO> queryDriverTeamAndGroup(@Param("cityIds") Set<String> cityIds, @Param("supplierIds") Set<String> supplierIds , @Param("teamIds") Set<Integer>  teamIds );
 
+	@SuppressWarnings("rawtypes")
+	@MasterSlaveConfigs(configs={
+			@MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+	} )
 	CarDriverTeam selectByCondition(DriverTeamRequest driverTeamRequest);
 
+	@SuppressWarnings("rawtypes")
+	@MasterSlaveConfigs(configs={
+			@MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+	} )
 	List<CarDriverTeam> queryForListByPid(DriverTeamRequest driverTeamRequest);
 
+	@SuppressWarnings("rawtypes")
+	@MasterSlaveConfigs(configs={
+			@MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+	} )
 	public Map<String, Object> queryTeamIdByDriverId(@Param("driverId") Integer driverId);
 
 	/** 车队设置中给车队排班制*/
--- src/main/java/mapper/mdbcarmanage/ex/CarRelateTeamExMapper.java
+++ src/main/java/mapper/mdbcarmanage/ex/CarRelateTeamExMapper.java
@@ -1,5 +1,8 @@
 package mapper.mdbcarmanage.ex;
 
+import com.zhuanche.common.database.DynamicRoutingDataSource;
+import com.zhuanche.common.database.MasterSlaveConfig;
+import com.zhuanche.common.database.MasterSlaveConfigs;
 import com.zhuanche.entity.mdbcarmanage.CarRelateTeam;
 import com.zhuanche.request.TeamGroupRequest;
 import org.apache.ibatis.annotations.Param;
@@ -10,10 +13,22 @@ import java.util.Set;
 
 public interface CarRelateTeamExMapper {
 
+    @SuppressWarnings("rawtypes")
+    @MasterSlaveConfigs(configs={
+            @MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+    } )
     List<CarRelateTeam> queryDriverTeamRelationList(TeamGroupRequest teamGroupRequest);
 
+    @SuppressWarnings("rawtypes")
+    @MasterSlaveConfigs(configs={
+            @MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+    } )
     List<Map<String,Object>> queryDriverIdsByTeamIds(@Param("teamIds") Set<Integer> teamIds);
 
+    @SuppressWarnings("rawtypes")
+    @MasterSlaveConfigs(configs={
+            @MasterSlaveConfig(databaseTag="mdbcarmanage-DataSource",mode= DynamicRoutingDataSource.DataSourceMode.SLAVE )
+    } )
     CarRelateTeam selectOneTeam(CarRelateTeam carRelateTeam);
 
 }
