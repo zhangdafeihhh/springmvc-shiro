3d9fa21956d65dd3a1db23a12d3b61169e49dc94
zhaoyali <zhaoyali@01zhuanche.com>
2018-09-21 14:19:08 +0800

    去除JDK包引入

--- src/main/java/com/zhuanche/serv/statisticalAnalysis/StatisticalAnalysisService.java
+++ src/main/java/com/zhuanche/serv/statisticalAnalysis/StatisticalAnalysisService.java
@@ -29,7 +29,6 @@ import org.springframework.stereotype.Service;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONArray;
 import com.alibaba.fastjson.JSONObject;
-import com.sun.tools.extcheck.Main;
 import com.zhuanche.common.web.AjaxResponse;
 import com.zhuanche.common.web.RestErrorCode;
 import com.zhuanche.http.HttpClientUtil;
