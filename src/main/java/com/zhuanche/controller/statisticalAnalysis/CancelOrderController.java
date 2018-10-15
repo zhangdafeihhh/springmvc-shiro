1e8d7b440e8a96c7e6d249c1e5c90d309f5f5ef7
lipengxiang <937845494@qq.com>
2018-10-15 18:07:12 +0800

    取消订单导出接口完成

--- src/main/java/com/zhuanche/controller/statisticalAnalysis/CancelOrderController.java
+++ src/main/java/com/zhuanche/controller/statisticalAnalysis/CancelOrderController.java
@@ -2,12 +2,16 @@ package com.zhuanche.controller.statisticalAnalysis;
 
 import java.io.File;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.zhuanche.dto.rentcar.CancelOrderDTO;
+import com.zhuanche.dto.rentcar.CancelOrderDetailDTO;
+import com.zhuanche.dto.rentcar.CompleteOrderDTO;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -130,7 +134,6 @@ public class CancelOrderController{
 	    
 	    /**
 		    * 导出取消订单列表
-		 	* @param 查询日期	queryDate
 		    * @param queryDate	查询日期
 			* @param driverCityId	司机所属城市ID
 			* @param allianceId	加盟商ID
@@ -140,9 +143,6 @@ public class CancelOrderController{
 			* @param orderVehicleTypeId	预约车型ID
 			* @param productTypeId	产品类型ID
 			* @param cancelDurationTypeId	取消时长分类ID
-			* @param visibleCityIds	可见城市ID
-			* @param visibleAllianceIds	可见加盟商ID
-			* @param visibleMotorcadeIds	可见车队ID
 		    * @return
 		  */
 	@ResponseBody
@@ -197,29 +197,113 @@ public class CancelOrderController{
 		      if(paramMap==null){
 		    	  return AjaxResponse.fail(RestErrorCode.HTTP_UNAUTHORIZED);
 		      }
-			String jsonString = JSON.toJSONString(paramMap);
-			 
-			statisticalAnalysisService.exportCsvFromToPage(
-					response,
-					jsonString,
-					saasBigdataApiUrl+"/cancelOrderDetail/download",
-					new String("取消订单分析".getBytes("gb2312"), "iso8859-1"),
-					request.getRealPath("/")+File.separator+"template"+File.separator+"cancelOrder_info.csv");
-			
+
+           //页号
+           paramMap.put("pageNo", 1);
+           //每页记录数
+           paramMap.put("pageSize", Integer.MAX_VALUE);
+
+           String jsonString = JSON.toJSONString(paramMap);
+           logger.info("【订单管理-统计分析】导出,完成订单详情请求参数----{}",jsonString);
+
+           List<CancelOrderDTO> completeOrderDTOList =  statisticalAnalysisService.queryCancelOrderDataList(saasBigdataApiUrl+"/cancelOrderDetail/queryList",paramMap, CancelOrderDTO.class);
+           String[] headers = new String[]{"下单城市", "创建时间", "预计乘车时间", "司机出发时间", "取消时间",
+                   "取消时长", "订单号", "司机ID", "司机类型", "加盟商名称", "司机姓名", "订车人ID", "预估金额",
+                   "下单渠道名称", "产品类型名称", "预定车型名称"};
+           String fileName = "取消订单详情Excel";
+           statisticalAnalysisService.exportExceleCancelOrder(completeOrderDTOList,response,headers,fileName);
+       } catch (Exception e) {
+           e.printStackTrace();
+       }
+       return AjaxResponse.success("成功");
+   }
+
+
+    /**
+     * 导出取消订单列表
+     * @param queryDate	查询日期
+     * @param driverCityId	司机所属城市ID
+     * @param allianceId	加盟商ID
+     * @param motorcadeId	车队ID
+     * @param driverTypeId	司机类型ID
+     * @param channelId	下单渠道ID
+     * @param orderVehicleTypeId	预约车型ID
+     * @param productTypeId	产品类型ID
+     * @param cancelDurationTypeId	取消时长分类ID
+     * @return
+     */
+    @ResponseBody
+    @RequestMapping(value = "/exportCancelOrderDataBak", method = { RequestMethod.POST,RequestMethod.GET })
+    public AjaxResponse exportCancelOrderDataBak(
+            Long driverCityId,
+            String allianceId,
+            String motorcadeId,
+            String driverTypeId,
+            String channelId,
+            String orderVehicleTypeId,
+            String productTypeId,
+            String cancelDurationTypeId,
+            String cancelTypeId,
+            @Verify(param = "queryDate",rule = "required") String queryDate,
+            HttpServletRequest request,
+            HttpServletResponse response){
+        logger.info("【运营管理-统计分析】导出,导出取消订单列表数据:exportCancelOrderData");
+        try {
+            Map<String, Object> paramMap = new HashMap<String, Object>();
+            if(null!=driverCityId){
+                paramMap.put("driverCityId", driverCityId);//司机所属城市ID
+            }
+            if(StringUtil.isNotEmpty(allianceId)){
+                paramMap.put("allianceId", allianceId);//加盟商ID
+            }
+            if(StringUtil.isNotEmpty(motorcadeId)){
+                paramMap.put("motorcadeId", motorcadeId);//车队ID
+            }
+            if(StringUtil.isNotEmpty(driverTypeId)){
+                paramMap.put("driverTypeId", driverTypeId);//司机类型ID
+            }
+            if(StringUtil.isNotEmpty(channelId)){
+                paramMap.put("channelId", channelId);//下单渠道ID
+            }
+            if(StringUtil.isNotEmpty(orderVehicleTypeId)){
+                paramMap.put("orderVehicleTypeId", orderVehicleTypeId);//预约车型ID
+            }
+            if(StringUtil.isNotEmpty(productTypeId)){
+                paramMap.put("productTypeId", productTypeId);//产品类型ID
+            }
+            if(StringUtil.isNotEmpty(cancelDurationTypeId)){
+                paramMap.put("cancelDurationTypeId", cancelDurationTypeId);//取消时长分类ID
+            }
+            if(StringUtil.isNotEmpty(cancelTypeId)){
+                paramMap.put("cancelTypeId", cancelTypeId);//取消类型
+            }
+            paramMap.put("queryDate", queryDate);//查询日期
+
+            // 数据权限设置
+            paramMap = statisticalAnalysisService.getCurrentLoginUserParamMap(paramMap,driverCityId,allianceId,motorcadeId);
+            if(paramMap==null){
+                return AjaxResponse.fail(RestErrorCode.HTTP_UNAUTHORIZED);
+            }
+            String jsonString = JSON.toJSONString(paramMap);
+
+            statisticalAnalysisService.exportCsvFromToPage(
+                    response,
+                    jsonString,
+                    saasBigdataApiUrl+"/cancelOrderDetail/download",
+                    new String("取消订单分析".getBytes("gb2312"), "iso8859-1"),
+                    request.getRealPath("/")+File.separator+"template"+File.separator+"cancelOrder_info.csv");
+
 		/*	statisticalAnalysisService.downloadCsvFromTemplet(jsonString,
 					saasBigdataApiUrl+"/cancelOrderDetail/download" ,
 					request.getRealPath("/")+File.separator+"template"+File.separator+"cancelOrder_info.csv");
 			statisticalAnalysisService.exportCsvFromTemplet(response,
 					new String("取消订单分析".getBytes("gb2312"), "iso8859-1"),
 					request.getRealPath("/")+File.separator+"template"+File.separator+"cancelOrder_info.csv");*/
-       } catch (Exception e) {
-           e.printStackTrace();
-       }
-       return AjaxResponse.success("成功");
-   }
-	
-	
-	
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return AjaxResponse.success("成功");
+    }
 	
 	
 	
