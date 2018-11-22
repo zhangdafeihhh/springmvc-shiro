7965df0e4baf25ef89f354d390bab89f0727a9a0
niuzilian <15510520468@163.com>
2018-11-22 15:56:19 +0800

    巴士车辆管理1

---
+++ src/main/java/com/zhuanche/controller/busManage/BusInfoController.java
@@ -0,0 +1,16 @@
+package com.zhuanche.controller.busManage;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @program: mp-manage
+ * @description: 巴士车辆管理
+ * @author: niuzilian
+ * @create: 2018-11-22 14:22
+ **/
+@RestController
+@RequestMapping("/busInfo")
+public class BusInfoController {
+
+}
---
+++ src/main/java/com/zhuanche/serv/busManage/BusInfoService.java
@@ -0,0 +1,11 @@
+package com.zhuanche.serv.busManage;
+
+/**
+ * @program: mp-manage
+ * @description:
+ * @author: niuzilian
+ * @create: 2018-11-22 15:51
+ **/
+public interface BusInfoService {
+
+}
