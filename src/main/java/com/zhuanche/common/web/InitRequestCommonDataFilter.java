9d5797654adb789252f4e0925e256e2e05cae8f8
yanyunpeng <yanyunpeng@01zhuanche.com>
2018-12-05 18:30:36 +0800

    Revert "测试登录"
    This reverts commit a6f9484513a7587735d8eff6a9329d8007d5d197.

--- src/main/java/com/zhuanche/common/web/InitRequestCommonDataFilter.java
+++ src/main/java/com/zhuanche/common/web/InitRequestCommonDataFilter.java
@@ -51,7 +51,7 @@ public class InitRequestCommonDataFilter extends OncePerRequestFilter {
 		}
         response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE, PUT");
         response.setHeader("Access-Control-Max-Age", "1800");
-        response.setHeader("Access-Control-Allow-Headers", "*");
+        response.setHeader("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept,XRequestedWith,LastModified");
         response.setHeader("Access-Control-Allow-Credentials", "true");		
 		/***************支持跨域请求END***********************/
         
