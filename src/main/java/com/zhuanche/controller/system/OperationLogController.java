9d503d922340b2bf045879909b7d532fac16f33c
yuxuan <yuxuan@01zhuanche.com>
2019-02-15 18:00:01 +0800

    新增菜单使用统计接口逻辑

--- src/main/java/com/zhuanche/controller/system/OperationLogController.java
+++ src/main/java/com/zhuanche/controller/system/OperationLogController.java
@@ -7,6 +7,7 @@ import com.zhuanche.util.DateUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.Date;
@@ -22,27 +23,37 @@ public class OperationLogController {
     @RequestMapping("/daily")
     @ResponseBody
     public AjaxResponse getUserDailyOperationCount(@Verify(param = "startDate",rule = "required") String startDate,
-                                                   @Verify(param = "endDate",rule = "required")String endDate){
-        Date start = DateUtils.parse(startDate, "yyyy-MM-dd", Date.class);
-        Date end = DateUtils.parse(endDate, "yyyy-MM-dd", Date.class);
-        return operationLogService.getUserDailyOperationCount(start, end);
+                                                   @Verify(param = "endDate",rule = "required")String endDate,
+                                                   @RequestParam(defaultValue = "20") int top){
+        Date start = DateUtils.parse(startDate + START_END_SUFFIX, FORMAT_STR, Date.class);
+        Date end = DateUtils.parse(endDate + END_END_SUFFIX, FORMAT_STR, Date.class);
+        return operationLogService.getUserDailyOperationCount(start, end, top);
     }
 
 
     @RequestMapping("/menu")
     @ResponseBody
-    public AjaxResponse getMenuOperationCount(@Verify(param = "startDate",rule = "required") Date startDate,
-                                              @Verify(param = "endDate",rule = "required")Date endDate){
-        return operationLogService.getMenuOperationCount(startDate, endDate);
+    public AjaxResponse getMenuOperationCount(@Verify(param = "startDate",rule = "required") String startDate,
+                                              @Verify(param = "endDate",rule = "required") String endDate,
+                                              @RequestParam(defaultValue = "20") int top){
+        return operationLogService.getMenuOperationCount(
+                DateUtils.parse(startDate + START_END_SUFFIX, FORMAT_STR, Date.class),
+                DateUtils.parse(endDate + END_END_SUFFIX, FORMAT_STR, Date.class),
+                top);
     }
 
     @RequestMapping("/user")
     @ResponseBody
-    public AjaxResponse getUserOperationCount(String startDate, String endDate){
-        System.out.println(startDate + endDate);
-        Date start = DateUtils.parse(startDate, "yyyy-MM-dd", Date.class);
-        Date end = DateUtils.parse(endDate, "yyyy-MM-dd", Date.class);
-        return operationLogService.getUserOperationCount(start, end);
+    public AjaxResponse getUserOperationCount(@Verify(param = "startDate",rule = "required") String startDate,
+                                              @Verify(param = "endDate",rule = "required") String endDate,
+                                              @RequestParam(defaultValue = "20") int top){
+        Date start = DateUtils.parse(startDate + START_END_SUFFIX, FORMAT_STR, Date.class);
+        Date end = DateUtils.parse(endDate + END_END_SUFFIX, FORMAT_STR, Date.class);
+        return operationLogService.getUserOperationCount(start, end, top);
     }
+
+    private static final String START_END_SUFFIX = " 00:00:00";
+    private static final String END_END_SUFFIX = " 23:59:59";
+    private static final String FORMAT_STR = "yyyy-MM-dd HH:mm:ss";
 
 }
