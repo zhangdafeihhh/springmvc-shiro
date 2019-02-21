e0d0fe41356993ec71e3f99fba70e17f50e08be3
yanyunpeng <yanyunpeng@01zhuanche.com>
2019-02-21 18:56:01 +0800

    保存司机城市 判断修改

--- src/main/java/com/zhuanche/controller/driver/DriverInfoController.java
+++ src/main/java/com/zhuanche/controller/driver/DriverInfoController.java
@@ -499,7 +499,7 @@ public class DriverInfoController {
         if (carBizSupplier == null) {
             return AjaxResponse.fail(RestErrorCode.SUPPLIER_NOT_EXIST, supplierId);
         }
-        if(serviceCity!=carBizSupplier.getSupplierCity()){
+        if(!carBizSupplier.getSupplierCity().equals(serviceCity)){
             return AjaxResponse.fail(RestErrorCode.CITY_SUPPLIER_DIFFER);
         }
         Boolean had = carBizCarInfoService.checkLicensePlates(licensePlates);
