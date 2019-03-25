ab81ae719fc5487eeac99eda807ae329739da787
fanhongtao <fanhongtao@01zhuanche.com>
2019-03-25 16:42:38 +0800

    修改下载的文件名

--- src/main/java/com/zhuanche/controller/tips/SupplierTipsController.java
+++ src/main/java/com/zhuanche/controller/tips/SupplierTipsController.java
@@ -30,6 +30,7 @@ import org.springframework.web.multipart.MultipartFile;
 import javax.servlet.http.HttpServletRequest;
 import java.io.File;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.text.MessageFormat;
 import java.util.UUID;
 
@@ -281,9 +282,10 @@ public class SupplierTipsController {
                 String downloadFielName = new String(fileName.getBytes("UTF-8"));
                 String docName = downloadFielName.substring(downloadFielName.lastIndexOf(File.separator)+1);
                 logger.info(docName);
+                headers.add("Content-Disposition", "attchement;filename="+ URLEncoder.encode(fileName,"UTF-8"));
 
                 //通知浏览器以attachment（下载方式）打开图片
-                headers.setContentDispositionFormData("attachment", docName);
+                //headers.setContentDispositionFormData("attachment", docName);
                 //application/octet-stream ： 二进制流数据（最常见的文件下载）。
                 headers.setContentType(MediaType.APPLICATION_OCTET_STREAM);
                 return new ResponseEntity<byte[]>(FileUtils.readFileToByteArray(file),
