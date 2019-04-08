fd828a13258c62f7ee4c7a0277a5cdbbc051f72b
zhulingling <zhulingling@01zhuanche.com>
2019-04-08 17:20:41 +0800

    添加根据用户权限查询供应商列表（包括无效）接口

--- src/main/java/com/zhuanche/controller/user/CitySupplierTeamController.java
+++ src/main/java/com/zhuanche/controller/user/CitySupplierTeamController.java
@@ -1,14 +1,5 @@
 package com.zhuanche.controller.user;
 
-import java.util.List;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-
 import com.zhuanche.common.web.AjaxResponse;
 import com.zhuanche.common.web.RestErrorCode;
 import com.zhuanche.common.web.Verify;
@@ -18,6 +9,14 @@ import com.zhuanche.serv.common.CitySupplierTeamService;
 import com.zhuanche.shiro.realm.SSOLoginUser;
 import com.zhuanche.shiro.session.WebSessionUtil;
 import com.zhuanche.util.Check;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /** 
  * 查询当前登录用户可见的城市、供应商、车队信息
@@ -35,8 +34,8 @@ public class CitySupplierTeamController {
 
 	@Autowired
     private CitySupplierTeamService citySupplierTeamService;
+
 	/** 查询当前登录用户可见的供应商信息 **/
-	
 	@RequestMapping("/suppliers")
     @ResponseBody
     public AjaxResponse getSuppliers(){
@@ -83,6 +82,23 @@ public class CitySupplierTeamController {
             return AjaxResponse.success(carDriverTeams);
         }catch (Exception e){
             logger.error("查询城市供应商车队列表异常:{}",e);
+            return AjaxResponse.fail(RestErrorCode.HTTP_SYSTEM_ERROR);
+        }
+    }
+
+    /** 查询当前登录用户可见的供应商信息,包括无效 **/
+    @RequestMapping("/suppliersAll")
+    @ResponseBody
+    public AjaxResponse getSuppliersAll(){
+        SSOLoginUser currentLoginUser = WebSessionUtil.getCurrentLoginUser();
+        if(Check.NuNObj(currentLoginUser)){
+            return AjaxResponse.fail(RestErrorCode.USER_NOT_EXIST);
+        }
+        try{
+            List<CarBizSupplier> carBizSuppliers = citySupplierTeamService.querySupplierAllList();
+            return AjaxResponse.success(carBizSuppliers);
+        }catch (Exception e){
+            logger.error("查询城市供应商列表异常:{}",e);
             return AjaxResponse.fail(RestErrorCode.HTTP_SYSTEM_ERROR);
         }
     }
