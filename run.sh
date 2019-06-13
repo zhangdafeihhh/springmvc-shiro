3534ba4525dfbd68c39caaca9d6d277afe14ffd4
suchao <persistsuchao@hotmail.com>
2019-06-13 17:59:20 +0800

    订单明细接口增加是否顺路单

--- run.sh
+++ run.sh
@@ -10,25 +10,25 @@ export PATH=$JAVA_HOME/bin:$PATH
 
 #2,start tomcat
 
-#if [ "${START_ENV}" = "dev" ]; then
-#   cd /u01/tomcat_docker_8080/webapps/ROOT
-#   cp -rfp ./WEB-INF/classes/dev/*  ./WEB-INF/classes/
-#fi
+if [ "${START_ENV}" = "dev" ]; then
+   cd /u01/tomcat_docker_8080/webapps/ROOT
+   cp -rfp ./WEB-INF/classes/dev/*  ./WEB-INF/classes/
+fi
 
-#if [ "${START_ENV}" = "test" ]; then
-#   cd /u01/tomcat_docker_8080/webapps/ROOT
-#   cp -rfp ./WEB-INF/classes/test/*  ./WEB-INF/classes/
-#fi
+if [ "${START_ENV}" = "test" ]; then
+   cd /u01/tomcat_docker_8080/webapps/ROOT
+   cp -rfp ./WEB-INF/classes/test/*  ./WEB-INF/classes/
+fi
 
-#if [ "${START_ENV}" = "pre" ]; then
-#   cd /u01/tomcat_docker_8080/webapps/ROOT
-#   cp -rfp ./WEB-INF/classes/pre/*  ./WEB-INF/classes/
-#fi
+if [ "${START_ENV}" = "pre" ]; then
+   cd /u01/tomcat_docker_8080/webapps/ROOT
+   cp -rfp ./WEB-INF/classes/pre/*  ./WEB-INF/classes/
+fi
 
-#if [ "${START_ENV}" = "online" ]; then
-#   cd /u01/tomcat_docker_8080/webapps/ROOT
-#   cp -rfp ./WEB-INF/classes/online/*  ./WEB-INF/classes/
-#fi
+if [ "${START_ENV}" = "online" ]; then
+   cd /u01/tomcat_docker_8080/webapps/ROOT
+   cp -rfp ./WEB-INF/classes/online/*  ./WEB-INF/classes/
+fi
 
 
 /u01/tomcat_docker_8080/bin/run.sh
