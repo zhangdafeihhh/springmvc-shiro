f82f18b51d1ab0c3b334b12e090cdc7327d840f9
xubaoyong <xubaoyong@01zhuanche.com>
2019-07-18 18:34:57 +0800

    供应商等级

---
+++ src/main/java/mapper/driver/SupplierLevelAdditionalMapper.java
@@ -0,0 +1,19 @@
+package mapper.driver;
+
+import com.zhuanche.entity.driver.SupplierLevelAdditional;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierLevelAdditionalMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SupplierLevelAdditional record);
+
+    int insertSelective(SupplierLevelAdditional record);
+
+    SupplierLevelAdditional selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SupplierLevelAdditional record);
+
+    int updateByPrimaryKey(SupplierLevelAdditional record);
+}
---
+++ src/main/java/mapper/driver/SupplierLevelMapper.java
@@ -0,0 +1,19 @@
+package mapper.driver;
+
+import com.zhuanche.entity.driver.SupplierLevel;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface SupplierLevelMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(SupplierLevel record);
+
+    int insertSelective(SupplierLevel record);
+
+    SupplierLevel selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(SupplierLevel record);
+
+    int updateByPrimaryKey(SupplierLevel record);
+}
