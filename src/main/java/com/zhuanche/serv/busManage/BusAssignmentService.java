a295df812533507888782abbd8ab2be96555c3b6
ghg <552006612@qq.com>
2019-07-23 19:44:19 +0800

    订单改派列表请求类型修改

--- src/main/java/com/zhuanche/serv/busManage/BusAssignmentService.java
+++ src/main/java/com/zhuanche/serv/busManage/BusAssignmentService.java
@@ -460,7 +460,7 @@ public class BusAssignmentService {
         logger.info("[ BusAssignmentService-selectList ] 请求参数   paramMap={}", paramMap);
 
         // 请求接口
-        JSONObject result = MpOkHttpUtil.okHttpGetBackJson(ORDER_API_URL + BusConst.Order.SELECT_ORDER_LIST, paramMap, 3000, "查询订单列表");
+        JSONObject result = MpOkHttpUtil.okHttpPostBackJson(ORDER_API_URL + BusConst.Order.SELECT_ORDER_LIST, paramMap, 3000, "查询订单列表");
         //String response = carRestTemplate.postForObject(BusConst.Order.SELECT_ORDER_LIST, JSONObject.class, paramMap);
         //JSONObject result = JSON.parseObject(response);
         int code = result.getIntValue("code");
