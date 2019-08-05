7ef9a5b3e7925c46dc2bb2edc74c92f1eead73c8
fanhongtao <fanhongtao@01zhuanche.com>
2019-08-05 15:30:24 +0800

    complier error

--- src/main/java/com/zhuanche/common/util/RedisKeyUtils.java
+++ src/main/java/com/zhuanche/common/util/RedisKeyUtils.java
@@ -12,7 +12,9 @@ public class RedisKeyUtils {
 
     public static final String VEHICLE_STATISTICS = "vehicle_statistics_"; //日均运营车辆统计查询key
 
-    public static final String SERVICE_RATE_STATISTIS = "order_statistics_"; //订单数量统计
+    public static final String ORDER_STATISTICS = "order_statistics_"; //订单数量统计
+
+    public static final String SERVICE_RATE_STATISTIS = "service_evaluation_rate_statistics_"; //服务差评率
 
     public static final String CORE_STATISTICS = "core_indicators_statistics"; //核心指标统计
 }
