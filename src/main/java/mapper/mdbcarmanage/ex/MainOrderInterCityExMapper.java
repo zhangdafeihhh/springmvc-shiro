eb713670c80d691efff41e52c8550a09e5f5f27d
fanhongtao <fanhongtao@01zhuanche.com>
2019-11-02 17:23:28 +0800

    修复编译失败

--- src/main/java/mapper/mdbcarmanage/ex/MainOrderInterCityExMapper.java
+++ src/main/java/mapper/mdbcarmanage/ex/MainOrderInterCityExMapper.java
@@ -14,6 +14,6 @@ public interface MainOrderInterCityExMapper {
 
     int updateMainOrderState(@Param("mainOrderNo") String mainOrderNo,
                              @Param("status") Integer status,
-                             @Param("phone") Integer phone);
+                             @Param("phone") String phone);
 
 }
