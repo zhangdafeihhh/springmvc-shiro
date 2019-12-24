df2fbd73bbc45a36351d3d4fa962092cfdb7fdcf
wangwenli <wangwenli@01zhuanche.com>
2019-12-24 18:00:22 +0800

    派单分增加列

--- src/main/java/com/zhuanche/dto/mdbcarmanage/ScoreDetailDTO.java
+++ src/main/java/com/zhuanche/dto/mdbcarmanage/ScoreDetailDTO.java
@@ -1,11 +1,16 @@
 package com.zhuanche.dto.mdbcarmanage;
 
+import lombok.Data;
+
+import java.math.BigDecimal;
+
 /**
  * @Author fanht
  * @Description
  * @Date 2019/8/8 下午4:37
  * @Version 1.0
  */
+@Data
 public class ScoreDetailDTO {
 
     private Integer driverId;
@@ -31,101 +36,17 @@ public class ScoreDetailDTO {
     private String finishOrderDay;//服务时长分计分周期（完单日）
 
     private String dispatchRollDay;//调度计分周期
+    //空闲时长分
+    private BigDecimal dayOfKongXianSC;
+    //特殊奖励分
+    private BigDecimal dayOfTeShuJL;
+    //新人时长分
+    private BigDecimal dayOfXinRenSC;
+    //调度分
+    private BigDecimal dayOfDiaoDu;
+    //应答分
+    private BigDecimal dayOfYingDa;
+    //该日服务时长分
+    private BigDecimal dayOfFuWuSC;
 
-
-    public Integer getDriverId() {
-        return driverId;
-    }
-
-    public void setDriverId(Integer driverId) {
-        this.driverId = driverId;
-    }
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getHourScore() {
-        return hourScore;
-    }
-
-    public void setHourScore(String hourScore) {
-        this.hourScore = hourScore;
-    }
-
-    public Integer getIsTotal() {
-        return isTotal;
-    }
-
-    public void setIsTotal(Integer isTotal) {
-        this.isTotal = isTotal;
-    }
-
-    public String getScoreDetailDate() {
-        return scoreDetailDate;
-    }
-
-    public void setScoreDetailDate(String scoreDetailDate) {
-        this.scoreDetailDate = scoreDetailDate;
-    }
-
-    public String getBaseTripScore() {
-        return baseTripScore;
-    }
-
-    public void setBaseTripScore(String baseTripScore) {
-        this.baseTripScore = baseTripScore;
-    }
-
-    public String getSumOfTripScore() {
-        return sumOfTripScore;
-    }
-
-    public void setSumOfTripScore(String sumOfTripScore) {
-        this.sumOfTripScore = sumOfTripScore;
-    }
-
-    public String getSumOfDispatchScore() {
-        return sumOfDispatchScore;
-    }
-
-    public void setSumOfDispatchScore(String sumOfDispatchScore) {
-        this.sumOfDispatchScore = sumOfDispatchScore;
-    }
-
-    public String getCalScoreDay() {
-        return calScoreDay;
-    }
-
-    public void setCalScoreDay(String calScoreDay) {
-        this.calScoreDay = calScoreDay;
-    }
-
-    public String getFinishOrderDay() {
-        return finishOrderDay;
-    }
-
-    public void setFinishOrderDay(String finishOrderDay) {
-        this.finishOrderDay = finishOrderDay;
-    }
-
-    public String getDispatchRollDay() {
-        return dispatchRollDay;
-    }
-
-    public void setDispatchRollDay(String dispatchRollDay) {
-        this.dispatchRollDay = dispatchRollDay;
-    }
 }
