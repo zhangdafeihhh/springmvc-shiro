d2167d4dd4d259ed764b4a30ff6a81995d5620db
fanhongtao <fanhongtao@01zhuanche.com>
2020-04-30 10:53:02 +0800

    修改默认值问题

--- src/main/java/com/zhuanche/serv/mdbcarmanage/service/impl/SupplierFeeRecordServiceImpl.java
+++ src/main/java/com/zhuanche/serv/mdbcarmanage/service/impl/SupplierFeeRecordServiceImpl.java
@@ -2,6 +2,7 @@ package com.zhuanche.serv.mdbcarmanage.service.impl;
 
 import com.zhuanche.entity.mdbcarmanage.SupplierFeeRecord;
 import com.zhuanche.serv.mdbcarmanage.service.SupplierFeeRecordService;
+import mapper.mdbcarmanage.SupplierFeeRecordMapper;
 import mapper.mdbcarmanage.ex.SupplierFeeRecordExMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -20,9 +21,12 @@ public class SupplierFeeRecordServiceImpl implements SupplierFeeRecordService{
     @Autowired
     private SupplierFeeRecordExMapper exMapper;
 
+    @Autowired
+    private SupplierFeeRecordMapper mapper;
+
     @Override
     public int insertFeeRecord(SupplierFeeRecord record) {
-        return exMapper.insertFeeRecord(record);
+        return mapper.insertSelective(record);
     }
 
     @Override
