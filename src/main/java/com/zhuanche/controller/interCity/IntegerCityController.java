dd16e7db7e755b7a746951e5fff4a5fb8d462b51
fanhongtao <fanhongtao@01zhuanche.com>
2020-05-08 20:29:21 +0800

    验签失败

--- src/main/java/com/zhuanche/controller/interCity/IntegerCityController.java
+++ src/main/java/com/zhuanche/controller/interCity/IntegerCityController.java
@@ -304,9 +304,7 @@ public class IntegerCityController {
         }
         //是否走平台流水
         if(payFlag != null && payFlag > 0){
-            map.put("payFlag",13);
-        }else {
-            map.put("payFlag",1);
+            map.put("payFlag","13");
         }
         if(StringUtils.isNotEmpty(lineName)){
             String ruleBatch = this.getRuleIdBatch(lineName);
