62bb9a374666197d617ec1759f461cecd2488985
fanhongtao <fanhongtao@01zhuanche.com>
2020-06-29 18:02:53 +0800

    修改编译错误

--- src/main/java/com/zhuanche/common/listen/NewInterCityListener.java
+++ src/main/java/com/zhuanche/common/listen/NewInterCityListener.java
@@ -18,6 +18,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
