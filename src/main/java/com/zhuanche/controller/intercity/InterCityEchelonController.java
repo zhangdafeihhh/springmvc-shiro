a6fee11023a8be4cda4e07bf859b1f331c7624fe
fanhongtao <fanhongtao@01zhuanche.com>
2020-07-28 20:12:27 +0800

    添加跨域注解

--- src/main/java/com/zhuanche/controller/intercity/InterCityEchelonController.java
+++ src/main/java/com/zhuanche/controller/intercity/InterCityEchelonController.java
@@ -21,10 +21,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.MessageFormat;
 import java.util.List;
@@ -37,6 +34,7 @@ import java.util.List;
  */
 @Controller
 @RequestMapping("/interCityEchelonController")
+@CrossOrigin
 public class InterCityEchelonController {
 
     private Logger logger = LoggerFactory.getLogger(this.getClass());
