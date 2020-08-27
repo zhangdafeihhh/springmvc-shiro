6784ad834ad9f5d7cd23644d673efce6dcc94856
fanhongtao <fanhongtao@01zhuanche.com>
2020-08-27 18:33:16 +0800

    修复订单评价日期异常、企业平台接入查询异常

--- src/main/java/com/zhuanche/common/web/HttpParamVerifyValidator.java
+++ src/main/java/com/zhuanche/common/web/HttpParamVerifyValidator.java
@@ -5,6 +5,7 @@ import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 import java.util.function.Predicate;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
 
@@ -90,6 +91,15 @@ public class HttpParamVerifyValidator {
 		return null;
 	}
 
+	public String verifyDateFormat(String value){
+		String rexp = "^((\\d{2}(([02468][048])|([13579][26]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])))))|(\\d{2}(([02468][1235679])|([13579][01345789]))[\\-\\/\\s]?((((0?[13578])|(1[02]))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(3[01])))|(((0?[469])|(11))[\\-\\/\\s]?((0?[1-9])|([1-2][0-9])|(30)))|(0?2[\\-\\/\\s]?((0?[1-9])|(1[0-9])|(2[0-8]))))))";
+ 		Pattern pat = Pattern.compile(rexp);
+		Matcher mat = pat.matcher(value);
+		if( ! mat.matches() ) {
+			return "传入值"+value+"，格式错误";
+		}
+		return null;
+	}
 	/**逗号分隔的数字**/
 	public String multiNumber( String value ) {
 		if(StringUtils.isEmpty(value) ){
